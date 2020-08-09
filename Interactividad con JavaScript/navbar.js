c9deadfce82c6153a2a3f50629c9424b0eb5ea89
Claudio Andres <ClaudioAndres03@outlook.com>
2020-08-09 14:40:43 -0400

    Esta parte pertenece a la interactividad con JavaScript

---
+++ Interactividad con JavaScript/navbar.js
@@ -0,0 +1,9 @@
+function accion(){
+    console.log('Esta funcionando mi boton');    
+    var ancla = document.getElementsByClassName('nav-enlace');
+
+    for(var i = 0; i < ancla.length; i++){
+        ancla[i].classList.toggle('desaparece');
+    }
+
+}
